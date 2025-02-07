0ffc97bbeb7f85882b1c97b3b7cfa60298d74d5b
prassivenky <bprassi@gmail.com>
2025-02-07 16:55:03 +0000

    Deploying to gh-pages from @ prassivenky/prassivenky.github.io@22d6ebddfeff6cf1ba748ac6c2a1ee1d87422a1f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-my-time-at-vw-wolfsburg-ended",
+          title: 'My time at VW Wolfsburg ended.',
+          description: "",
+          section: "News",},{id: "news-master-thesis-research-on-decoupling-between-different-calibration-error-estimators-accepted-at-gcpr-vmv-2024-sparkles",
+          title: 'Master thesis research on decoupling between different calibration error estimators accepted at GCPR-VMV...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
